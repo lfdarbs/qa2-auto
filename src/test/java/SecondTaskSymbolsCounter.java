4e914bb6a7bfce6c1e7caa406b2383214222ea15
Sergejs_Vorobjovs <lfdarbs@gmail.com>
2020-11-13 18:38:48 +0200

    Added home work 2st task

---
+++ src/test/java/SecondTaskSymbolsCounter.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class SecondTaskSymbolsCounter {
+
+//Тестовая фраза
+    String str = ("Сколько тут символов?");
+//  String str = new String("Сколько тут символов?");
+
+    @Test
+    public void symbolCounter() {
+//подсчет символов включая пробел
+        int count = 0;
+        for(int i = 0; i<str.length(); i++) {
+            count++;}
+
+//подсчет слов
+        int blockCount = str.split(" ").length;
+
+//вывод результатов
+        System.out.println("Тестовая фраза: " + str);
+        System.out.println("Количество символов = " + count);
+        System.out.println("Количество слов = " + blockCount);
+    }
+}
